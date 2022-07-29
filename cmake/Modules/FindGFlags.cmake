9467a727cd937e64a9d25c0dc70e4c0a47c5ea3e
cutegirl520 <160213624+cutegirl520@users.noreply.github.com>
2022-07-29 11:09:28 +0000

    Initial commmit

---
+++ cmake/Modules/FindGFlags.cmake
@@ -0,0 +1,9 @@
+# - Try to find GFLAGS
+#
+# The following variables are optionally searched for defaults
+#  GFLAGS_ROOT_DIR:            Base directory where all GFLAGS components are found
+#
+# The following are set after configuration is done:
+#  GFLAGS_FOUND
+#  GFLAGS_INCLUDE_DIRS
+#  G
