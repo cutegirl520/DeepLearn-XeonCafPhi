a13538cb9b26552e6d8e98c97d2ac20aaa44b05c
cutegirl520 <160213624+cutegirl520@users.noreply.github.com>
2023-03-14 16:24:10 +0000

    New translations 01_speech.md (Spanish)

---
+++ include/caffe/vision_layers.hpp
@@ -0,0 +1,21 @@
+#ifndef CAFFE_VISION_LAYERS_HPP_
+#define CAFFE_VISION_LAYERS_HPP_
+
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/common.hpp"
+#include "caffe/common_layers.hpp"
+#include "caffe/data_layers.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/loss_layers.hpp"
+#include "caffe/neuron_layers.hpp"
+#include "caffe/proto/caffe.pb.h"
+
+namespace caffe {
+
+/**
+ * @brief Abstract base class that factors out the BLAS code common to
+ *        ConvolutionLayer and Deconvolutio
