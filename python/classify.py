2da2aa28dace6f59dc3649a5af537103e613a6c8
cutegirl520 <160213624+cutegirl520@users.noreply.github.com>
2023-04-22 19:18:46 +0000

    commit1

---
+++ python/classify.py
@@ -0,0 +1,13 @@
+
+#!/usr/bin/env python
+"""
+classify.py is an out-of-the-box image classifer callable from the command line.
+
+By default it configures and runs the Caffe reference ImageNet model.
+"""
+import numpy as np
+import os
+import sys
+import argparse
+import glob
+import time
