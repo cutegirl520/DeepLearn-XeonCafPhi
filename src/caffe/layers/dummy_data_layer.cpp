e99191e6e1d4407289afbf3495d6894aec52b673
cutegirl520 <160213624+cutegirl520@users.noreply.github.com>
2023-05-12 16:02:06 +0000

    Translation updates from translatewiki.net

--- src/caffe/layers/dummy_data_layer.cpp
+++ src/caffe/layers/dummy_data_layer.cpp
@@ -89,4 +89,28 @@ void DummyDataLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
       top[i]->Reshape(param.shape(shape_index));
     }
   }
-  // Run Forward once, with refill_ inve
+  // Run Forward once, with refill_ inverted, to fill the constant Blobs.
+  this->Forward(bottom, top);
+  // Invert the inverted refill_ values to refill the desired (non-constant)
+  // Blobs in every usual forward pass.
+  for (int i = 0; i < refill_.size(); ++i) {
+    refill_[i] = !refill_[i];
+  }
+}
+
+template <typename Dtype>
+void DummyDataLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) {
+  LOG(INFO) << "Forward_cpu in DummyDataLayer";
+  for (int i = 0; i < top.size(); ++i) {
+    const int filler_id = (fillers_.size() > 1) ? i : 0;
+    if (refill_[filler_id]) {
+      fillers_[filler_id]->Fill(top[i]);
+    }
+  }
+}
+
+INSTANTIATE_CLASS(DummyDataLayer);
+REGISTER_LAYER_CLASS(DummyData);
+
+}  // namespace caffe
---
+++ src/caffe/layers/hdf5_data_layer.cpp
@@ -0,0 +1,22 @@
+
+/*
+TODO:
+- load file in a separate thread ("prefetch")
+- can be smarter about the memcpy call instead of doing it row-by-row
+  :: use util functions caffe_copy, and Blob->offset()
+  :: don't forget to update hdf5_daa_layer.cu accordingly
+- add ability to shuffle filenames if flag is set
+*/
+#include <fstream>  // NOLINT(readability/streams)
+#include <string>
+#include <vector>
+
+#include "hdf5.h"
+#include "hdf5_hl.h"
+#include "stdint.h"
+
+#include "caffe/data_layers.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/util/io.hpp"
+
+namespace caffe {
