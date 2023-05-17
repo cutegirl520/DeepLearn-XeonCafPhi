0f2b3c82034a58716e0bf76ba51a2a993317d51d
cutegirl520 <160213624+cutegirl520@users.noreply.github.com>
2023-05-17 10:02:25 +0000

    Script fixes

---
+++ src/caffe/layers/memory_data_layer.cpp
@@ -0,0 +1,15 @@
+#include <opencv2/core/core.hpp>
+
+#include <vector>
+
+#include "caffe/data_layers.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/util/io.hpp"
+
+namespace caffe {
+
+template <typename Dtype>
+void MemoryDataLayer<Dtype>::DataLayerSetUp(const vector<Blob<Dtype>*>& bottom,
+     const vector<Blob<Dtype>*>& top) {
+  batch_size_ = this->layer_param_.memory_data_param().batch_size();
+  cha
