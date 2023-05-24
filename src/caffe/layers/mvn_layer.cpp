ec823df65952335e155a4fa1c28b31348103c1b3
cutegirl520 <160213624+cutegirl520@users.noreply.github.com>
2023-05-24 22:07:59 +0000

    added licenses

---
+++ src/caffe/layers/mvn_layer.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <vector>
+
+#include "caffe/common_layers.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/util/math_functions.hpp"
+
+namespace caffe {
+
+template <typename Dtype>
+void MVNLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) {
+  top[0]->Reshape(bottom[0]->num(), bottom[0]->channels(),
+      bottom[0]->height(), bottom[0]->width());
+  mean_.Reshape(bottom[0]->num(), bottom[0]->channels(),
+      1, 1);
+  variance_.Reshape(bottom[0]->num(), bottom[0]->channels(),
+      1, 1);
+  temp_.Reshape(bottom[0]->num(), bottom[0]->channels(),
+      bottom[0]->height(), bottom[0]->width());
+  sum_multiplier_.Reshape(1, 1,
+      bottom[0]->height(), bottom[0]->width());
+  Dtype* multiplier_data = sum_multiplier_.mutable_cpu_data();
+  caffe_set(sum_multiplier_.count(), Dtype(1), multiplier_data);
+}
+
+template <typename Dtype>
+void MVNLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+    const vector<Blob<Dtype>*>& top) {
+  const Dtype* bottom_data = bottom[0]->cpu_data();
+  Dtype* top_data = top[0]->mut
