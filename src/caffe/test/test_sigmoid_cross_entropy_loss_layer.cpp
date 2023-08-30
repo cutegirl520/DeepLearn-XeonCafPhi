cd94aeb729e1777a0031ff4fcd6f31580c2d560e
cutegirl520 <160213624+cutegirl520@users.noreply.github.com>
2023-08-30 07:02:51 +0000

    add backup

---
+++ src/caffe/test/test_sigmoid_cross_entropy_loss_layer.cpp
@@ -0,0 +1,72 @@
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <vector>
+
+#include "gtest/gtest.h"
+
+#include "caffe/blob.hpp"
+#include "caffe/common.hpp"
+#include "caffe/filler.hpp"
+#include "caffe/vision_layers.hpp"
+
+#include "caffe/test/test_caffe_main.hpp"
+#include "caffe/test/test_gradient_check_util.hpp"
+
+namespace caffe {
+
+template <typename TypeParam>
+class SigmoidCrossEntropyLossLayerTest : public MultiDeviceTest<TypeParam> {
+  typedef typename TypeParam::Dtype Dtype;
+
+ protected:
+  SigmoidCrossEntropyLossLayerTest()
+      : blob_bottom_data_(new Blob<Dtype>(10, 5, 1, 1)),
+        blob_bottom_targets_(new Blob<Dtype>(10, 5, 1, 1)),
+        blob_top_loss_(new Blob<Dtype>()) {
+    // Fill the data vector
+    FillerParameter data_filler_param;
+    data_filler_param.set_std(1);
+    GaussianFiller<Dtype> data_filler(data_filler_param);
+    data_filler.Fill(blob_bottom_data_);
+    blob_bottom_vec_.push_back(blob_bottom_data_);
+    // Fill the targets vector
+    FillerParameter targets_filler_param;
+    targets_filler_param.set_min(0);
+    targets_filler_param.set_max(1);
+    UniformFiller<Dtype> targets_filler(targets_filler_param);
+    targets_filler.Fill(blob_bottom_targets_);
+    blob_bottom_vec_.push_back(blob_bottom_targets_);
+    blob_top_vec_.push_back(blob_top_loss_);
+  }
+  virtual ~SigmoidCrossEntropyLossLayerTest() {
+    delete blob_bottom_data_;
+    delete blob_bottom_targets_;
+    delete blob_top_loss_;
+  }
+
+  Dtype SigmoidCrossEntropyLossReference(const int count, const int num,
+                                         const Dtype* input,
+                                         const Dtype* target) {
+    Dtype loss = 0;
+    for (int i = 0; i < count; ++i) {
+      const Dtype prediction = 1 / (1 + exp(-input[i]));
+      EXPECT_LE(prediction, 1);
+      EXPECT_GE(prediction, 0);
+      EXPECT_LE(target[i], 1);
+      EXPECT_GE(target[i], 0);
+      loss -= target[i] * log(prediction + (target[i] == Dtype(0)));
+      loss -= (1 - target[i]) * log(1 - prediction + (target[i] == Dtype(1)));
+    }
+    return loss / num;
+  }
+
+  void TestForward() {
+    LayerParameter layer_param;
+    const Dtype kLossWeight = 3.7;
+    layer_param.add_loss_weight(kLossWeight);
+    FillerParameter data_filler_param;
+    data_filler_param.set_std(1);
+    GaussianFiller<Dtype> data_filler(data_filler_param);
+    FillerParameter targets_filler_param;
+    targets
